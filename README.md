2e8472d374c49c1c1a3227749c8a8d6b762ff965
Stijn Gijsen <gonewacko@gonewacko.com>
2015-05-14 21:34:07 +0200

    Update YouTubePlugin to use YouTube Data API v3
    Now requires the isodate and google-api-python-client packages to
    function.

--- README.md
+++ README.md
@@ -14,4 +14,8 @@ The available plugins are:
 * **ChannelLinkPlugin**: Sends messages to members of the *admin* groups of the relevant channels when channels are linked and unlinked. 
 * **TopicPlugin**: Allows users to modify the `welcometext` (aka MOTD) by using the `!addtopic` and `!deltopic` commands. **Warning:** HTML is allowed!
 * **TwitchPlugin**: Looks up stream information (Channel name, stream title, number of viewers, game being played, thumbnail) for Twitch.tv streams when users send messages containing links to twitch.tv, and posts the information to the mumble channel(s).
-* **YouTubePlugin**: Similar to `TwitchPlugin`, but for YouTube. Looks up video name, duration, channel name, and thumbnail and posts them to the mumble channel(s).
+* **YouTubePlugin**: Similar to `TwitchPlugin`, but for YouTube. Looks up video name, duration, channel name, and thumbnail and posts them to the mumble channel(s). Has some configuration settings in `plugins/youtube.py`.
+
+The YouTube Plugin requires the following libraries:
+* [isodate](https://pypi.python.org/pypi/isodate)
+* [google-api-python-client](https://developers.google.com/api-client-library/python/apis/youtube/v3)
--- plugins/youtube.py
+++ plugins/youtube.py
@@ -1,21 +1,47 @@
 import re
 import urlparse
-import urllib2
-import xml.etree.ElementTree as ET
+import isodate
+
+from apiclient.discovery import build
+from apiclient.errors import HttpError
 
 from Plugin import Plugin
 
-href_re = 'href="([^"]*)"'
+# Get a "Server Key" for Public API Access from the Google Developer Console and paste it here:
+YOUTUBE_API_KEY = "<YOUR API KEY HERE>"
+
+# Set to True if video durations should also be retrieved.
+# Incurs an extra API quota cost of 2 units per request
+YOUTUBE_INCLUDE_DURATION = True
+# Set to True if a clickable video thumbnail should be included.
+# No extra API cost, simply links to a .jpg on the YouTube servers.
+YOUTUBE_INCLUDE_THUMBNAIL = True
+# If True, and YOUTUBE_INCLUDE_THUMBNAIL is True above, the thumbnails will be
+# clickable links to the video. Currently the thumbnail will not use the exact
+# link as it appeared in the user's message, which may cause some information
+# to be lost, such as if the user's link was to a specific time in the video.
+YOUTUBE_THUMBNAIL_CLICKABLE = True
+
+# End of configuration options. Don't modify anything below this line.
 
 class YouTubePlugin(Plugin):
+    href_re = 'href="([^"]*)"'
+
+    class YouTubeError(Exception):
+        def __init__(self, msg):
+            self.msg = msg
+        def __str__(self):
+            return 'YouTube API Error: ' + self.msg
 
     def __init__(self, server, adapter):
         self.server = server
         self.adapter = adapter
+        self.youtube = build("youtube", "v3", developerKey=YOUTUBE_API_KEY)
 
-    def get_ids(self, message):
+    def get_video_ids(self, message):
+        "Returns the set of video IDs appearing in YouTube links in a message."
         ids = set()
-        uris = re.findall(href_re, message)
+        uris = re.findall(self.href_re, message)
         for uri in uris:
             i = None
             res = urlparse.urlparse(uri)
@@ -26,61 +52,67 @@ class YouTubePlugin(Plugin):
             elif res.netloc in ['www.youtu.be', 'youtu.be']:
                 i = re.match('^/(?P<vid>.*)', res.path).group('vid')
             if i is not None:
-                ids.add((i, uri))
+                ids.add(i)
         return ids
 
-    def get_title(self, vid):
-        video = {}
-        url = "http://gdata.youtube.com/feeds/api/videos/" + vid
-        request = urllib2.Request(url)
-        response = urllib2.urlopen(request, timeout=2)
-        if response.getcode() == 200:
-            tree = ET.parse(response)
-            t = tree
-            channel = tree.find('./{http://www.w3.org/2005/Atom}author/{http://www.w3.org/2005/Atom}name')
-            if channel is None:
-                video['error'] = 'No channel name found'
-                return video
-            else:
-                video['channel'] = channel.text
-            group = tree.find('./{http://search.yahoo.com/mrss/}group')
-            if group is None:
-                video['error'] = 'No media:group found'
-                return video
-            else:
-                title = group.find('./{http://search.yahoo.com/mrss/}title')
-                if title is None:
-                    video['error'] = 'No title found'
-                    return video
-                else:
-                    video['title'] = title.text
-                duration = group.find('./{http://gdata.youtube.com/schemas/2007}duration')
-                if duration is None:
-                    video['error'] = 'No duration found'
-                    return video
-                else:
-                    seconds = int(duration.get('seconds'))
-                    minutes = seconds / 60
-                    video['duration'] = "%d minutes %02d seconds" % (minutes, seconds % 60)
-                thumb = group.find('./{http://search.yahoo.com/mrss/}thumbnail')
-                if thumb is not None:
-                    video['thumb'] = thumb.get('url')
+    def get_videos(self, ids):
+        ids = set(ids)
+        results = {}
+        if len(ids) == 0:
+            return results
+        if YOUTUBE_INCLUDE_DURATION:
+            parts="id,snippet,contentDetails"
         else:
-            video['error'] = response.read()
-        return video
+            parts="id,snippet"
+        request = self.youtube.videos().list(part=parts, id=",".join(ids))
+        try:
+            response = request.execute()
+        except HttpError, err:
+            raise YouTubePlugin.YouTubeError(err.content)
+        return response['items']
 
-    def process(self, message, vid, url):
-        video = self.get_title(vid)
-        if 'error' in video:
-            for c in message.channels:
-                self.server.sendMessageChannel(c, False, video['error'])
+    def sendReply(self, message, text):
+        for c in message.channels:
+            self.server.sendMessageChannel(c, False, text)
+
+    def timedelta_to_string(self, delta):
+        hours, remainder = divmod(delta.seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        results = []
+        if (hours > 0):
+            results.append("%d hours" % (hours))
+        if (minutes > 0):
+            results.append("%d minutes" % (minutes))
+        if (seconds > 0):
+            results.append("%d seconds" % (seconds))
+        return ", ".join(results)
+
+    def process(self, message, video):
+        title = video['snippet']['title']
+        channel = video['snippet']['channelTitle']
+        if YOUTUBE_INCLUDE_DURATION:
+            duration = isodate.parse_duration(video['contentDetails']['duration'])
+            text = '<p><b><span style="color: red;">YouTube</span></b> video: <b>%s</b> (%s) by <b>%s</b></p>' % (title, self.timedelta_to_string(duration), channel)
         else:
-            text = '<p><b><span style="color: red;">YouTube</span></b> video: <b>%s</b> (%s) by <b>%s</b></p>' % (video['title'], video['duration'], video['channel'])
-            if 'thumb' in video:
-                text += '<p><a href="%s"><img src="%s" height="180" width="240"></a></p>' % (url, video['thumb'])
-            for c in message.channels:
-                self.server.sendMessageChannel(c, False, text)
+            text = '<p><b><span style="color: red;">YouTube</span></b> video: <b>%s</b> by <b>%s</b></p>' % (title, channel)
+        if YOUTUBE_INCLUDE_THUMBNAIL:
+            thumbnail = video['snippet']['thumbnails']['medium']
+            thumb_width = thumbnail['width']
+            thumb_height = thumbnail['height']
+            thumb_url = thumbnail['url']
+            if YOUTUBE_THUMBNAIL_CLICKABLE:
+                url = "https://www.youtube.com/watch?v=" + video['id']
+                text += '<p><a href="%s"><img src="%s" width="%d" height="%d"></a></p>' % (url, thumb_url, thumb_width, thumb_height)
+            else:
+                text += '<p><img src="%s" width="%d" height="%d"></p>' % (thumb_url, thumb_width, thumb_height)
+        self.sendReply(message, text)
 
     def userTextMessage(self, user, message, current=None):
-        for i, uri in self.get_ids(message.text):
-            self.process(message, i, uri)
+        ids = self.get_video_ids(message.text)
+        if len(ids) > 0:
+            try:
+                videos = self.get_videos(ids)
+                for video in videos:
+                    self.process(message, video)
+            except YouTubePlugin.YouTubeError, err:
+                self.sendReply(message, str(err))
